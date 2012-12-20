1b9fe3bb182e93dfd15af11e130bf3f9129f35c2
blueplanet <erguolinge@gmail.com>
2012-12-21 07:10:04 +0900

    Add if != None

--- AutoRevealInSideBar.py
+++ AutoRevealInSideBar.py
@@ -3,4 +3,5 @@ import sublime_plugin
  
 class AutoRevealInSideBar(sublime_plugin.EventListener):
     def on_activated(self, view):
-        view.window().run_command("reveal_in_side_bar")
+        if view != None:
+            view.window().run_command("reveal_in_side_bar")
