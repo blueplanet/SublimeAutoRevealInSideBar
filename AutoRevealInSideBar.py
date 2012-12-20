97407cf29a6d88a958e11f532d33b0151bcd90cd
blueplanet <erguolinge@gmail.com>
2012-12-21 06:14:53 +0900

    init

---
+++ AutoRevealInSideBar.py
@@ -0,0 +1,6 @@
+import sublime
+import sublime_plugin
+ 
+class AutoRevealInSideBar(sublime_plugin.EventListener):
+    def on_activated(self, view):
+        view.window().run_command("reveal_in_side_bar")
